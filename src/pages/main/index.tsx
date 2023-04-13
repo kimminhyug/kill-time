e61800a2f8920d365fcc1221f4068145ac9c47cf
unknown <kimminhyug@naver.com>
2023-04-13 20:35:08 +0900

    baseurl 추가 main 화면에 dynamic import 추가

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,14 +1,18 @@
 import { Box } from "@mui/material";
 import { commonSX } from "../common/styles";
 import Menu from "./menu/menu";
+import { useRouter } from "next/router";
+import Content from "../content";
 const Main = () => {
+  const router = useRouter();
+
   return (
     <Box sx={commonSX} component="div" className="app-container">
       <Box sx={commonSX} component="div" className="menu">
         <Menu />
       </Box>
       <Box sx={commonSX} component="div" className="content-container">
-        contents
+        <Content />
       </Box>
     </Box>
   );
